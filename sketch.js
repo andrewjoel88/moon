db6dfe39477155bdb47a8f4689299a83a911962c
Andrew Johnson <andrewjoel88@gmail.com>
2026-01-22 13:35:25 -0800

    sphere that rotates

--- sketch.js
+++ sketch.js
@@ -1,30 +1,11 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  describe('A cube you can look around by clicking and dragging');
 }
-
 function draw() {
-  background(0);
-  //phaseAnimation + 0.1;
-  //phaseAnimation = phaseAnimation % 30;
-for(let i = 0; i < 30; i++){  
-  drawMoon(phaseAnimation);
+  background(220);
+ 
+  orbitControl();
+  fill(255, 0, 0);
+  sphere(50);
 }
-  //text("moonphase: " +phaseAnimation, width/2, 100);
-  fill(0)
-  ellipse(width/ellipse, height/2, 50, 50);
-  //drawMoon(5);
-  //drawMoon(mouseX, mouseY);
-}
-
-// function drawTarget(x, y) {
-
-//   ellipse (x, y, 50, 50);
-//   ellipse (x, y, 30, 30); 
-// }
-
-function drawMoon(phase) { //phase 0-30
-  fill(255, 255, 0)
-  let xLoc = map(phase, 0, 30, width/2-30, width/2+30);
-  ellipse(xLoc, height/2, 50, 50);
-}
-
